22506b7abf09986dd26a8018ff991fa7809f4cff
CaioMorato100 <110397868+CaioMorato100@users.noreply.github.com>
2022-08-01 10:58:08 -0300

    upd
    Programando

--- CHERNOBYL TCC PJD/Assets/Scripts/PlayerScript.cs
+++ CHERNOBYL TCC PJD/Assets/Scripts/PlayerScript.cs
@@ -10,10 +10,13 @@ public class PlayerScript : MonoBehaviour
     [Header("Atirar")]
     public GameObject projetil;
     public GameObject pontoDeTiro;
-    
+    private Animator anim;
+    private bool verificarDirecao;
+
     void Start()
     {
         rB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -24,13 +27,32 @@ public class PlayerScript : MonoBehaviour
         transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime * movimentoHorizontal);
         transform.Translate(new Vector3(0, 1, 0) * speed * Time.deltaTime * movimentoVertical);
 
+        if(movimentoHorizontal > 0 && verificarDirecao == true)
+        {
+            flipX();
+        }
+        if (movimentoHorizontal < 0 && verificarDirecao == false)
+        {
+            flipX();
+        }
+
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             Instantiate(projetil, pontoDeTiro.transform.position, projetil.transform.rotation);
         }
-       
-        }
+        anim.SetInteger("Horizontal", (int)movimentoHorizontal);
     }
+    void flipX()
+    {
 
-      
+        verificarDirecao = !verificarDirecao;
+        float x = transform.localScale.x * -1;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+    }
+    
+}
+
+
+
 
